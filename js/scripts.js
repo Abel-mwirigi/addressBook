9b16efed9b71a25d8ac2ea699d944807b982b221
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2024-05-02 12:56:24 +0300

    initial javascripts logic

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+// UI Logic
+$(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+        event.preventDefault();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newcontact = new contact(inputtedFirstName, inputtedLastName);
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    });
+});
